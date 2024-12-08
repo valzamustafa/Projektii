f492a81f861bf51b6f1f22c9b9bb3cc3dad9419e
altinakrapi <altinakrapi10@gmail.com>
2024-12-08 15:15:42 +0200

    Contact

---
+++ Contact.js
@@ -0,0 +1,30 @@
+function validateForm() {
+   
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var message = document.getElementById('message').value;
+
+
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if (!email.match(emailPattern)) {
+        alert("Ju lutem shkruani një email të vlefshëm.");
+        return false;
+    }
+
+
+    var phonePattern = /^\+?[0-9]{1,4}?[0-9]{7,10}$/;
+    if (!phone.match(phonePattern)) {
+        alert("Ju lutem shkruani një numër telefoni të vlefshëm.");
+        return false;
+    }
+
+
+    if (name == "" || message == "") {
+        alert("Ju lutem plotësoni të gjitha fushat.");
+        return false;
+    }
+
+    alert("Mesazhi u dërgua me sukses!");
+    return true; 
+}
