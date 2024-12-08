95b0774b4d17b3fff3b709f82c244df561adde09
valzamustafa <vm70019@ubt-uni.net>
2024-12-08 11:26:53 +0100

    style js

---
+++ style.js
@@ -0,0 +1,27 @@
+console.log("Featured Cars Page Loaded");
+
+
+
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener('click', () => {
+    container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener('click', () => {
+    container.classList.remove("sign-up-mode");
+});
+
+
+
+window.addEventListener("scroll", () => {
+    const header = document.querySelector("header");
+    if (window.scrollY > 50) {
+        header.classList.add("transparent");
+    } else {
+        header.classList.remove("transparent");
+    }
+});
+
