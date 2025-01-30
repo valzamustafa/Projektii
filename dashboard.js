f79138b5098f2fb016b98460ad0b8a2aa7156666
valzamustafa <vm70019@ubt-uni.net>
2025-01-30 17:29:23 +0100

    dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -2,7 +2,7 @@ function showSidebar() {
     const sidebar = document.querySelector('.slidebar');
     sidebar.style.display = 'flex'; 
     }
-    
+
     function hideSideBar() {
     const sidebar = document.querySelector('.slidebar');
     sidebar.style.display = 'none'; 
@@ -13,3 +13,29 @@ function showSidebar() {
         document.getElementById("mainFrame").src = page;
     }
 
+    document.getElementById("showCarForm").addEventListener("click", function() {
+        var container = document.getElementById("carFormContainer");
+
+        if (container.innerHTML.trim() === "") {
+
+            fetch("cars.php?formOnly=true")
+                .then(response => response.text())
+                .then(data => {
+                    container.innerHTML = data;
+                    container.style.display = "block";
+                })
+                .catch(error => console.error("Gabim në ngarkimin e formës:", error));
+        } else {
+
+            container.style.display = (container.style.display === "none") ? "block" : "none";
+        }
+    });
+    function showSidebar() {
+        const sidebar = document.querySelector('.slidebar');
+        sidebar.style.display = 'flex'; 
+        }
+
+        function hideSideBar() {
+        const sidebar = document.querySelector('.slidebar');
+        sidebar.style.display = 'none'; 
+        }
