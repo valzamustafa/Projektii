b755396dbe30ae1e134fc9a818f3e6817b13b3c7
altinakrapi <altinakrapi10@gmail.com>
2025-02-01 22:44:17 +0100

    ndryshim

--- users.php
+++ users.php
@@ -90,14 +90,15 @@ if (!$result) {
         </ul>
     </nav>
 
-    
     <div class="sidebar">
         <h2>Car Dealership - Admin Panel</h2>
         <ul>
+
             <li><a href="users.php">Menaxho Përdoruesit</a></li>
             <li><a href="cars.php">Menaxho Makinat</a></li>
-     
             <li><a href="manage_contacts.php">Menaxho Mesazhet</a></li>
+            <li><a href="add_content.php">Menaxho Përmbajtjen e About Us</a></li>
+            <li><a href="manage_news.php">Menaxho News</a></li>
         </ul>
     </div>
   
